a3756927439a587a327103143b7d67c80ec8b2a7
PrunusSerrulata <58555471+PrunusSerrulata@users.noreply.github.com>
2024-06-18 22:00:23 +0100

    update presets.py

--- packager/presets.py
+++ packager/presets.py
@@ -3,13 +3,15 @@ import re
 ignore = re.compile(
     (r"dol-with-ml/|"
      r"packager/|"
+     r"wiki/|"
      r"\.zip$|"
      r"\.bat$|"
+     r"\.sh$|"
      r"^\.")
 )
 
 name = "ARedSecret"
-version = "-"
+version = "0.0.1-alpha"
 
 additionFile = [ "assetsList.json" ]
 additionDir = [ "assets" ]
